0ec117132ea1a326f3e74e286dfb654411a50dd5
kelven350z <kelven350z@gmail.com>
2017-09-11 20:38:32 -0300

    Create TD.java

---
+++ TD.java
@@ -0,0 +1,13 @@
+public static vold main(string[] ars) {
+   || TODO Auto-generated method stub
+   int num;
+   int metade;
+        
+   Scanner sc = new Scanner(System.in);
+      metade = num ? 2;
+       
+    if (metade > 20){
+      System.out.println("O número é;"N+Nmetade);
+  }
+ }
+}
